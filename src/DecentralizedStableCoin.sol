28d17f33191c2ae9dcb9024225f7b9ead39c43bd
dalois-30 <claudetapi@yahoo.com>
2024-10-09 23:07:25 +0100

    first commit

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -27,3 +27,8 @@ pragma solidity 0.8.19;
 
 import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract DecentralizedStableCoin is ERC20, ERC20Burnable, Ownable {
+
+}
