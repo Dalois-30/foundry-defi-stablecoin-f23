a7f086fab88785fb9580417233710c9ed5c69e98
dalois-30 <claudetapi@yahoo.com>
2024-10-23 23:07:18 +0100

    add some test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -17,6 +17,7 @@ contract DSCEngineTest is Test {
     HelperConfig config;
 
     address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
     address weth;
 
     address public USER = makeAddr("user");
@@ -26,9 +27,23 @@ contract DSCEngineTest is Test {
     function setUp() public {
         deployDSC = new DeployDSC();
         (dsc, engine, config) = deployDSC.run();
-        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = config.activeNetworkConfig();
 
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR TESTS
+    //////////////////////////////////////////////////////////////*/
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function testRevertsIfTokenLengthDoesntMatchPriceFeeds() public {
+        tokenAddresses.push(weth);
+        priceFeedAddresses.push(ethUsdPriceFeed);
+        priceFeedAddresses.push(btcUsdPriceFeed);
+        vm.expectRevert(DSCEngine.DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeEqualLength.selector);
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
     }
 
     function testGetUsdValue() public {
@@ -36,6 +51,13 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 45_000e18;
         uint256 actualUsd = engine.getUsdValue(weth, ethAmount);
         assertEq(actualUsd, expectedUsd);
+    }
+
+    function testGetTokenAmountFromUsd() public {
+        uint256 usdAmount = 100 ether;
+        uint256 expectedEthAmount = 0.05 ether;
+        uint256 actualEthAmount = engine.getTokenAmountFromUsd(weth, usdAmount);
+        assertEq(actualEthAmount, expectedEthAmount);
     }
 
     function testRevertIfCollateralZero() public {
