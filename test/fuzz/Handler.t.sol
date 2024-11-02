4a2c050a570bab7f02aa8809c2e8650095990395
dalois-30 <claudetapi@yahoo.com>
2024-11-02 22:30:55 +0100

    resolve error insufficient allowance

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,19 +6,45 @@ pragma solidity 0.8.19;
 import { Test } from "forge-std/Test.sol";
 import { DSCEngine } from "../../src/DSCEngine.sol";
 import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
+import { ERC20Mock } from '@openzeppelin/contracts/mocks/ERC20Mock.sol';
+// import { ERC20Mock } from "../mocks/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine engine;
     DecentralizedStableCoin dsc;
 
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
     constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
         engine = _dscEngine;
         dsc = _dsc;
+
+        address[] memory collateralTokens = engine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
     }
 
     // redeem collateral <-
-    function depositCollateral(address collateral, uint256 amountCollateral) public {
-        engine.depositCollateral(collateral, amountCollateral);
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(engine), amountCollateral); // resolve the insufficient allowance error
+        engine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    // Helper Functions
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed %2 == 0) {
+            return weth;
+        }
+        return wbtc;
     }
 
 }
